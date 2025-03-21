1bc94d95d8bcc4ce49fcae7bc441ada8a09d923e
thetrokers <luisleos90@gmail.com>
2025-03-21 17:37:31 -0600

    yo se que esta viendo esto profe, si chambee

--- don_galleto/main/urls.py
+++ don_galleto/main/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from main.views import main, welcome
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', main, name='home'),
-    path('welcome/', welcome, name='welcome' )
-]
+    path('welcome', welcome, name='welcome'),
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

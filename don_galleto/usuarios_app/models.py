71ed5c31bb18c72d9f808d2c697760c99811a6bb
Alonsiwisky <alonvillaram123@outlook.com>
2025-03-22 13:01:04 -0600

    crud usuarios terminado

--- don_galleto/usuarios_app/models.py
+++ don_galleto/usuarios_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+#modelo de usaurios
+class Usuario(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=10)
+    user_type = models.CharField(max_length=10, default='usuario')
 
 

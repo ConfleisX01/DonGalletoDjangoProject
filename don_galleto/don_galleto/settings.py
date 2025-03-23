392525efed5284ab8a19333d5c58130cb121b65b
antony20033 <tonyvaladez65@gmail.com>
2025-03-23 04:00:14 -0600

    CRUD acabado, diseno casi acabado de appRecetas

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'panel',
+    'Recetas_app',
 ]
 
 MIDDLEWARE = [
--- don_galleto/don_galleto/urls.py
+++ don_galleto/don_galleto/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('panel/', include('panel.urls')),
+    path('Receta/', include('Recetas_app.urls')) # SE AGREGO LA URL PARA LA RECETA HASTA SABER COMO SE VA HACER CON EL PANEL
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

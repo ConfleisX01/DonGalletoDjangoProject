ae874b643689a94d1d3a9c226f0071b3f79d1b03
thetrokers <luisleos90@gmail.com>
2025-03-25 07:33:41 -0600

    Login con con un poco de diseño funcional, tambien el crear usuarios para iniciar sesion

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -136,4 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'welcome'
-LOGOUT_REDIRECT_URL = 'main'
+LOGOUT_REDIRECT_URL = 'principal'
--- don_galleto/don_galleto/urls.py
+++ don_galleto/don_galleto/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from main.views import principal, panel, welcome
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('accounts/registro/', views.registro, name="registro"),
+    path('panel', panel, name='panel'),
+    path('principal', principal, name='principal'),
+    path('welcome', welcome, name='welcome'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

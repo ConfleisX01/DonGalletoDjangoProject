f4b05860dc4346f31f69f72198a1f0b1bf980e0a
Alonsiwisky <alonvillaram123@outlook.com>
2025-03-25 23:52:12 -0600

    cambios menores

--- don_galleto/don_galleto/urls.py
+++ don_galleto/don_galleto/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('usuarios/', include('usuarios_app.urls')),
     path('panel/', include('panel.urls')),
-<<<<<<< HEAD
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
     path('provedores/', include('provedores.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> provedores

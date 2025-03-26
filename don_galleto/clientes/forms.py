7276a071bf3e57f975358d7da91a52d63fa72b4f
JuanPablo01 <blackstar371@outlook.com>
2025-03-26 02:57:47 -0600

    Correcciones de diseño y detalles

--- don_galleto/clientes/forms.py
+++ don_galleto/clientes/forms.py
@@ -3,6 +3,16 @@ from django import forms
 from clientes.models import Cliente
 
 class ClienteCrearForm(forms.ModelForm):
+    first_name = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombres'})
+    )
+
+    last_name = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellidos'})
+    )
+
     username = forms.CharField(
         max_length=150,
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario'})
@@ -20,6 +30,8 @@ class ClienteCrearForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = User.objects.create_user(
+            first_name = self.cleaned_data['first_name'],
+            last_name = self.cleaned_data['last_name'],
             username=self.cleaned_data['username'],
             email=self.cleaned_data['email'],
             password=self.cleaned_data['password']
@@ -30,23 +42,37 @@ class ClienteCrearForm(forms.ModelForm):
         return cliente
 
 class ClienteEditarForm(forms.ModelForm):
+    first_name = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombres'})
+    )
+
+    last_name = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellidos'})
+    )
+
     username = forms.CharField(
         max_length=150,
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario'})
     )
+
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo electrónico'})
     )
+
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}),
     )
 
     class Meta:
         model = Cliente
-        fields = ['username', 'email', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
 
     def save(self):
         user = self.instance.user
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.username = self.cleaned_data['username']
         user.email  = self.cleaned_data['email']
         user.password = self.cleaned_data['password']

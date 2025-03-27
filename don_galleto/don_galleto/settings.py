4aceccd2f60cc3fccf9223b94d8942d61b643252
antony20033 <tonyvaladez65@gmail.com>
2025-03-27 17:44:58 -0600

    hola

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dongalleto',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'poponacas123',
         'HOST': 'localhost',
         'PORT': '3306',
     }

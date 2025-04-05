c765a2667489567302b9ea8aa54a00188f1e6ced
Juan Pablo Perez Fernandez <blackstar371@outlook.com>
2025-04-04 18:53:37 -0600

    Corecciones del Merge

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dongalleto',
         'USER': 'root',
-        'PASSWORD': 'Antonioleos1',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -169,8 +169,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/welcome/'
-LOGOUT_REDIRECT_URL = '/welcome/'
+LOGIN_REDIRECT_URL = '/welcome'
+LOGOUT_REDIRECT_URL = '/welcome'
 
 AUTHENTICATION_BACKENDS = (
     'axes.backends.AxesBackend',  # Habilita el control de accesos
--- don_galleto/inventarios/models.py
+++ don_galleto/inventarios/models.py
@@ -30,7 +30,6 @@ class InventarioProducto(models.Model):
 
 class InventarioMaterial(models.Model):
     insumo = models.OneToOneField(MateriaPrima, on_delete=models.CASCADE)
-<<<<<<< HEAD
     cantidad = models.PositiveBigIntegerField(default=500, null=False, blank=False)
     ultima_actualizacion = models.DateTimeField(auto_now=True)
 
@@ -42,7 +41,6 @@ class Merma(models.Model):
 
     def __str__(self):
         return f"Merma - Lote {self.lote.id} ({self.lote.galleta.nombre_galleta}) - {self.fecha.strftime('%Y-%m-%d')}"
-=======
     cantidad = models.PositiveBigIntegerField(default=0, null=False, blank=False)
     ultima_actualizacion = models.DateTimeField(auto_now=True)
 
@@ -64,4 +62,3 @@ class Merma(models.Model):
 
     def __str__(self):
         return f"Nombre insumo: {self.insumo.nombre_insumo} - Cantidad: {self.cantidad} - Ultima vez acualizado: {self.ultima_actualizacion}"
->>>>>>> dbe1769c46235201b08c444271476005dd0056a8

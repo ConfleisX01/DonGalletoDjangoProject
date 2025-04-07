1ab8db65b9bd24f29a7c961fa99775698ed2d419
JuanPablo01 <blackstar371@outlook.com>
2025-04-07 14:09:54 -0600

    Mejoras en la UI

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -170,8 +170,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/cuentas/login/'
-LOGIN_REDIRECT_URL = '/welcome/'
-LOGOUT_REDIRECT_URL = '/welcome/'
+LOGIN_REDIRECT_URL = 'welcome'
+LOGOUT_REDIRECT_URL = 'welcome'
 
 AUTHENTICATION_BACKENDS = (
     'axes.backends.AxesBackend',  # Habilita el control de accesos
